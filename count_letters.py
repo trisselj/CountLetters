317310cc98f4dd4d67f9ac4f2d8a47bc1fc41126
trisselj <trisselj@oregonstate.edu>
2024-08-07 11:48:38 -0700

    a

--- count_letters.py
+++ count_letters.py
@@ -6,7 +6,12 @@
 def count_letters(s):
     letter_counts = {} # Initialize empty dictionary for letter counting
     for char in s: # Iterates through every character in the text
-        upper_char = char.upper() # Converts lower case to upper case for ease of counting
+        if 'a' <= char <= 'z':  # Checks if the character is a lowercase letter
+            upper_char = chr(ord(char) - 32)  # Convert to uppercase using ASCII values
+        elif 'A' <= char <= 'Z':  # Checks if the character is already an uppercase letter
+            upper_char = char
+        else:
+            continue # Continues with code
         if 'A' <= upper_char <= 'Z': # Defines reading for characters only
             if upper_char in letter_counts: # If character already counted add 1
                 letter_counts[upper_char] += 1 
